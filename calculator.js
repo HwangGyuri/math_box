7547987992773903c626f95cd751b2cb2b25d441
rbfl3721 <try528@hanyang.ac.kr>
2026-07-14 09:32:13 +0900

    feat: update calculator.js

--- calculator.js
+++ calculator.js
@@ -5,3 +5,10 @@ add = (a, b) => a + b;
 substract = (a, b) => a - b;
 
 const multiply = (a, b) => a * b;
+
+const divide = (a, b) => {
+  if (b === 0) {
+    throw new Error();
+  }
+  return a / b;
+};
